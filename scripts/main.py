a1c7561822cd32e0429f299575be4b5fd7d28796
Rajaram Kaliyaperumal <rr.kaliyaperumal@gmail.com>
2021-08-25 16:41:20 +0200

    new key added

--- scripts/main.py
+++ scripts/main.py
@@ -1,4 +1,4 @@
 import os
 print("Hello this is FDP populator script")
-TEST_SECRET = os.environ['GREETING']
-print("TEST_SECRET :" + TEST_SECRET)
+DEMO_KEY = os.environ['DEMO_KEY']
+print("DEMO_KEY :" + DEMO_KEY)
