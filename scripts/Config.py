19d251c9e5415e438638d02d06ae9213b9fbcf63
Rajaram Kaliyaperumal <rr.kaliyaperumal@gmail.com>
2021-08-27 10:36:50 +0200

    Take input file as param

--- scripts/Config.py
+++ scripts/Config.py
@@ -7,4 +7,4 @@ FDP_URL = os.environ['FDP_URL']
 FDP_USERNAME = os.environ['FDP_USERNAME']
 FDP_PASSWORD = os.environ['FDP_PASSWORD']
 FDP_PERSISTENT_URL = os.environ['FDP_PERSISTENT_URL']
-INPUT_FILE = "../catalogs.csv"
+INPUT_FILE = os.environ['INPUT_FILE']
