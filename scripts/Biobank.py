3c236cfd7ddb4711f88c1ef4c7fa021b8106d0d8
jdwijnbergen <j.d.wijnbergen1@gmail.com>
2022-09-28 09:11:30 +0200

    Added biobank class

---
+++ scripts/Biobank.py
@@ -0,0 +1,29 @@
+class Biobank():
+    """
+    This class describes the biobank class
+    """
+    PARENT_URL = None
+    TITLE = None
+    DESCRIPTION = None
+    THEMES = []
+    LANDING_PAGE = None
+    TYPE = None
+
+
+    def __init__(self, parent_url, title, description, themes, page, type):
+        """
+
+        :param parent_url: Parent's catalog URL of a biobank. NOTE this url should exist in an FDP
+        :param title: Title of a biobank
+        :param description: Description of a biobank
+        :param themes: Themes URLs to describe a biobank
+        :param publisher: Publisher URL of a biobank (e.g. https://orcid.org/0000-0002-1215-167X)
+        :param page: Landing page URL of a biobank
+        :param type: type of a biobank
+        """
+        self.PARENT_URL = parent_url
+        self.TITLE = title
+        self.DESCRIPTION = description
+        self.THEMES = themes
+        self.LANDING_PAGE = page
+        self.TYPE = type
